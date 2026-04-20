9490343d731ba3e6204371991968684ba19a0b6a
shilpaKothe <shilpakothe17@gmail.com>
2026-04-20 11:30:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Portfolio Loaded");
+
+// Example: Alert on contact click
+document.querySelector("#contact").addEventListener("click", () => {
+    console.log("Contact section opened");
+});
